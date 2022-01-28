00242ad59b634bd07e6230a7cb51417c950fb9d7
penmats <56763620+penmats@users.noreply.github.com>
2022-01-28 17:01:48 +0000

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,51 +1,67 @@
-import json
+import sys
 import sqlite3
 import pandas as pd
 
-# with open('uncommitted/Posts.json') as f:
-#      json.load(f)
+# Validate number of arguments passed
+n = len(sys.argv)
+if n!=3:
+   print("Invalid arguments passed.")
+   sys.exit(1)
 
-Posts =pd.read_json ('uncommitted/Posts.json')
-Votes =pd.read_json ('uncommitted/Votes.json')
+
+def chk_conn(conn):
+    try:
+        conn.cursor()
+        return True
+    except Exception as ex:
+        return False
+
+myconn= sqlite3.connect('warehouse.db')
+
+# Check database connection
+if chk_conn(myconn)!=True:
+    print("Database does not exists")
+
+
+# Read the json files and load data to dataframes
+
+Posts =pd.read_json(sys.argv[1])
+Votes =pd.read_json(sys.argv[2])
 
 Postsdf=pd.DataFrame.from_dict(Posts, orient='columns')
-Votesdf=pd.DataFrame.from_dict(Posts, orient='columns')
+Votesdf=pd.DataFrame.from_dict(Votes, orient='columns')
 
 
-con= sqlite3.connect('warehouse.db')
+# Load dataframes to tables
 
-Postsdf.to_sql("Posts",con=con,if_exists='replace')
-Votesdf.to_sql("Votes",con=con,if_exists='replace')
+Postsdf.to_sql("Poststbl",con=myconn,if_exists='replace')
+Votesdf.to_sql("Votestbl",con=myconn,if_exists='replace')
 
+# Calculate mean votes per post per week
+cur = myconn.cursor()
 
-#
-# con= sqlite3.connect('warehouse.db')
-# colnames=[]
-#
-# for row in data:
-#     colnames.extend(row.keys())
-# con.execute(f"create table  if not exists test {tuple(set(colnames))}")
-#
-# cursor = con.cursor()
-# for row in data:
-#     keys = ','.join(row.keys())
-#     question_marks = ','.join(list('?'*len(row)))
-#     values = tuple(row.values())
-#     # print('INSERT INTO test ('+keys+') VALUES ('+question_marks+')', values)
-#     cursor.execute('INSERT INTO test ('+keys+') VALUES ('+question_marks+')', values)
-#
+# Number of votes
+cur.execute("select count(*) from Poststbl p inner join Votestbl v on p.Id=v.PostId")
+y=cur.fetchone()[0]
+print(y)
 
 
+# Number of posts
+Totalposts=cur.execute("select count(distinct p.Id) from Poststbl p inner join Votestbl v on p.Id=v.PostId")
+z=cur.fetchone()[0]
+print(z)
 
 
-# # read the json file
-# data = json.loads(f.read())
-#
-# # Iterating through the json
-# # list
-# for i in data:
-#     print(i)
+# Number of weeks
+Totalweeks=cur.execute("select count(distinct strftime('%W%Y',v.CreationDate)) from Poststbl p inner join Votestbl v on p.Id=v.PostId")
+p=cur.fetchone()[0]
+print(p)
 
-# Closing file
-# f.close()
+Mean=((y/z)/p)
+print("Mean of votes per post per week is",Mean)
+
+
+
+
+
 
